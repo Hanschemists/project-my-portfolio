dc7b744ae54c4ef0f840fe24da01793ae438a116
NineMzz <Hanschemists@gmail.com>
2024-12-28 15:39:41 +0700

    Add TabsList and custom tabs in UI

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -136,9 +136,38 @@ const skills = {
 
 const resume = () => {
     return (
-        <div>
-            <h1>Resume</h1>
-        </div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{
+                opacity: 1,
+                transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+            }}
+            className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+        >
+            <div className="container mx-auto">
+                <Tabs
+                    defaultValue="experience"
+                    className="flex flex-col xl:flex-row gap-[60px]"
+                >
+                    <TabsList
+                        className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-4"
+                    >
+                        <TabsTrigger value="experience">Experience</TabsTrigger>
+                        <TabsTrigger value="education">Education</TabsTrigger>
+                        <TabsTrigger value="skills">Skills</TabsTrigger>
+                        <TabsTrigger value="about">About Me</TabsTrigger>
+                    </TabsList>
+
+                    {/* Content */}
+                    <div className="min-h-[70vh] w-full">
+                        {/* Experience */}
+                        <TabsContent value="experience" className="w-full">
+                            Experience
+                        </TabsContent> 
+                    </div>
+                </Tabs>
+            </div>
+        </motion.div>
     )
 };
 
